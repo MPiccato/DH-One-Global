6b202d273040c165674612821b858c6b57fea049
MP Data - Trading <mpiccato@me.com>
2026-07-16 17:34:16 -0300

    armando el menu

--- FrontEnd/NextJS/app-demo/app/components/menu/Menu.tsx
+++ FrontEnd/NextJS/app-demo/app/components/menu/Menu.tsx
@@ -1,6 +1,12 @@
 const Menu = () => {
     return <>
         <nav>
+            <ul className="mb-2">
+                <li className="text-xl mb-2 hover:border-b-2 border-blue-400" >Inicio</li>
+                <li className="text-xl mb-2 hover:border-b-2 border-blue-400">Perfil</li>
+                <li className="text-xl mb-2 hover:border-b-2 border-blue-400" >Explorar</li>
+            </ul>
+            <button className='button-primary'>Postear</button>
 
         </nav>
     </>
