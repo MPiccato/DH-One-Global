865e9336fe45541d31c20ca4b0b4bf62dcb32169
MP Data - Trading <mpiccato@me.com>
2026-07-16 18:10:42 -0300

    terminada la fase estética del menú

--- FrontEnd/NextJS/app-demo/app/components/menu/Menu.tsx
+++ FrontEnd/NextJS/app-demo/app/components/menu/Menu.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 const Menu = () => {
     return <>
-        <nav>
+        <nav className="flex flex-col">
             <ul className="mb-2">
-                <li className="text-xl mb-2 hover:border-b-2 border-blue-400" >Inicio</li>
-                <li className="text-xl mb-2 hover:border-b-2 border-blue-400">Perfil</li>
-                <li className="text-xl mb-2 hover:border-b-2 border-blue-400" >Explorar</li>
+                <li className="text-xl mb-2 hover:border-b-2 border-blue-400 w-full">
+                    <Link href="/" className="flex w-full px-2">Inicio</Link>
+                </li>
+                <li className="text-xl mb-2 hover:border-b-2 border-blue-400 w-full">
+                    <Link href="/" className="flex w-full px-2">Perfil</Link>
+                </li>
+                <li className="text-xl mb-2 hover:border-b-2 border-blue-400 w-full">
+                    <Link href="/" className="flex w-full px-2">Explorar</Link>
+                </li>
             </ul>
             <button className='button-primary'>Postear</button>
 
