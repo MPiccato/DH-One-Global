91a7a4b0d6988e622a576fd9b9037b288e8745f5
MP Data - Trading <mpiccato@me.com>
2026-08-30 06:11:33 -0300

    iniciando register

---
+++ FrontEnd/NextJS/app-demo/app/register/page.tsx
@@ -0,0 +1,14 @@
+import RegisterForm from "../components/auth/RegisterForm";
+
+
+const RegisterPage = () => {
+  return (
+    <div className="flex justify-center w-full">
+        <h2 className="mb-2">Registro a la Red Social</h2>
+        <RegisterForm />
+
+    </div>
+  )
+}
+
+export default RegisterPage;
